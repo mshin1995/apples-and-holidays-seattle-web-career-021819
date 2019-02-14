1adf9d61f0cf432bbac9072685fe26a6bc5738a8
Matthew Shin <mshin1995@gmail.com>
2019-02-14 09:16:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,8 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize
 end
 
 def all_holidays_with_bbq(holiday_hash)
