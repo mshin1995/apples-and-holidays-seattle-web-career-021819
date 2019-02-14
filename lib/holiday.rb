f84888439937ad57c5a118eab93e37589c619f18
Matthew Shin <mshin1995@gmail.com>
2019-02-14 09:27:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}: "
     holiday.each do |holiday, item|
       puts "#{holiday.to_s.split.collect do |x|
-        x
+        x.capitalize
   
     
 end
