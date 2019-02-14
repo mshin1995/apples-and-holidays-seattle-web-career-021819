ed2cb20fdc83ff8ce64ae1093667248dfb61c894
Matthew Shin <mshin1995@gmail.com>
2019-02-14 09:10:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,8 +38,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].each do |holiday, item|
-    puts item
+  array = []
+  holiday_hash[:winter].collect do |holiday, item|
+    item
   end
 end
 
