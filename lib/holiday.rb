f5dddb358aa35b068c7604e96dbd2862571d8723
Matthew Shin <mshin1995@gmail.com>
2019-02-14 09:11:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].collect do |holiday, item|
     array << item
   end
-  array
+  array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
